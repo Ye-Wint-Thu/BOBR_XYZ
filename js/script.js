8ce30b563ca57fe36305e7907edaf6b0d070cc14
Ye-Wint-Thu <40794501@live.napier.ac.uk>
2025-10-07 18:36:09 +0700

    Updated some of the files.

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
     let lightMode = document.getElementById('light-mode');
     const body = document.body;
 
-            const savedTheme = localStorage.getItem("theme");
-            if (savedTheme === "dark") {
-                body.classList.add("dark-mode");
-                themes.style.rotate = "180deg";
-            }
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        body.classList.add("dark-mode");
+        themes.style.rotate = "180deg";
+    }
 
-            darkMode.addEventListener("click", () => {
-                body.classList.add("dark-mode");
-                themes.style.rotate = "180deg";
-                localStorage.setItem("theme", "dark");
-            });
+    darkMode.addEventListener("click", () => {
+        body.classList.add("dark-mode");
+        themes.style.rotate = "180deg";
+        localStorage.setItem("theme", "dark");
+    });
 
-            lightMode.addEventListener("click", () => {
-                body.classList.remove("dark-mode");
-                themes.style.rotate = "0deg";
-                localStorage.setItem("theme", "light");
-            });
+    lightMode.addEventListener("click", () => {
+        body.classList.remove("dark-mode");
+        themes.style.rotate = "0deg";
+        localStorage.setItem("theme", "light");
+    });
 });
